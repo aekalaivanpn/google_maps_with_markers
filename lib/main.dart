3fa3c4a5ef1fc790ecf91df7bfac7d54af7e01c6
Aekalaivan <aekalaivanpn@gmail.com>
2021-11-28 19:57:33 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -45,64 +45,123 @@ class _MapScreenState extends State<MapScreen> {
         onTap: () {
           _customInfoWindowController.addInfoWindow!(
             Container(
-                height: double.infinity,
-                width: double.infinity,
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(0.0, 4.0),
+                      blurRadius: 10.0,
+                    ),
+                  ]),
+              child: Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black54,
-                        offset: Offset(0.0, 4.0),
-                        blurRadius: 10.0,
-                      ),
-                    ]),
-                child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.white,
-                    ),
-                  child: Row(
+                  color: Colors.white,
+                ),
+                child: Row(
                   children: [
-                      Container(
-                        height: 60.0,
-                        width: 60.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10.0),
-                            topLeft: Radius.circular(10.0),
-
+                    Container(
+                      height: 120.0,
+                      width: 105.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10.0),
+                          topLeft: Radius.circular(10.0),
+                        ),
+                        image: DecorationImage(
+                            image: NetworkImage(element.thumbnail),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                    SizedBox(width: 3.0),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 8.0),
+                          Text(
+                            element.name,
+                            style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'sans-serif'),
                           ),
-                          image: DecorationImage(
-                              image: NetworkImage(element.thumbnail),
-                              fit: BoxFit.cover),
-                        ),
+                          SizedBox(height: 5.0),
+                          Text(
+                            element.address,
+                            style: TextStyle(
+                                fontSize: 13.0,
+                                fontWeight: FontWeight.w500,
+                                fontFamily: 'San Francisco'),
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.amber,
+                              ),
+                              Text(
+                                element.description,
+                                style: TextStyle(
+                                    fontSize: 17.0,
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: 'San Francisco'),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 7.0),
+                          Divider(thickness: 1.0,height: 1.0),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: CircleAvatar(
+                                  foregroundImage: NetworkImage(
+                                      'https://e7.pngegg.com/pngimages/915/706/png-clipart-blue-call-icon-dialer-android-google-play-telephone-phone-blue-text.png'),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: CircleAvatar(
+                                  foregroundImage: NetworkImage(
+                                      'https://news.wirefly.com/sites/phonedog.com/files/styles/blog_entry/public/android-messages-icon.jpg?itok=pVmsm2T3'),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: CircleAvatar(
+                                  foregroundImage: NetworkImage(
+                                      'https://w7.pngwing.com/pngs/672/164/png-transparent-whatsapp-icon-whatsapp-logo-computer-icons-zubees-halal-foods-whatsapp-text-circle-unified-payments-interface-thumbnail.png'),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: CircleAvatar(
+                                  foregroundImage: NetworkImage(
+                                      'https://i.stack.imgur.com/rRITT.png'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      SizedBox(width: 3.0),
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(element.name,
-                              style: TextStyle(
-                                  fontSize: 12.5, fontWeight: FontWeight.bold),
-                            ),
-                            Text(element.address,
-                              style: TextStyle(
-                                  fontSize: 10.0, fontWeight: FontWeight.w600),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
+            ),
             element.coordinates,
           );
         },
-      )
-      );
+      ));
     });
   }
 
@@ -136,9 +195,9 @@ class _MapScreenState extends State<MapScreen> {
                 ),
               ),
               CustomInfoWindow(
-                controller: _customInfoWindowController!,
-                height: 75,
-                width: 150,
+                controller: _customInfoWindowController,
+                height: 130,
+                width: 300,
                 offset: 50,
               ),
             ],
--- lib/places.dart
+++ lib/places.dart
@@ -17,56 +17,51 @@ class Places {
 
 final List<Places> placesOfInterest = [
   Places(
-    name: 'Marudhamalai',
-    address: 'Coimbatore - 641 046',
-    description:
-    'Lord Murugan temple built by Tamil kings during the Sangam period.',
+    name: 'Madhumitha',
+    address: 'Senior Manager',
+    description: '3.2/5',
     thumbnail:
-    'https://upload.wikimedia.org/wikipedia/commons/e/eb/Maruthamalai_Rajagopuram.jpg',
+    'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small_2x/beautiful-latin-woman-avatar-character-icon-free-vector.jpg',
     coordinates: LatLng(11.046111, 76.851944),
   ),
   Places(
-    name: 'Siruvani Waterfalls',
-    address: 'Coimbatore - 641 114',
-    description:
-    'Siruvani Waterfalls and the dam named after them are located 36 km in the Western Ghats.',
+    name: 'Manoj Jayabalan',
+    address: 'Senior Executive',
+    description: '4.5/5',
     thumbnail:
-    'https://upload.wikimedia.org/wikipedia/commons/9/98/Siruvani_Falls_above_bathing_area.jpg',
+    'https://t4.ftcdn.net/jpg/02/23/50/73/240_F_223507324_jKl7xbsaEdUjGr42WzQeSazKRighVDU4.jpg',
     coordinates: LatLng(10.938011, 76.687177),
   ),
   Places(
-    name: 'Prozone Mall',
-    address: 'Coimbatore - 641 035',
-    description:
-    'Prozone Mall is one of the largest vertically designed shopping malls, covering over 500,000 sq ft.',
+    name: 'Stella Mary',
+    address: 'Assistant Manager',
+    description: '4.3/5',
     thumbnail:
-    'https://www.ourcities.in/wp-content/uploads/2020/05/lifestyle-Prozone-mall-coimbatore.jpg',
+    'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small_2x/beautiful-latin-woman-avatar-character-icon-free-vector.jpg',
     coordinates: LatLng(11.05523, 76.993875),
   ),
   Places(
-    name: 'Black Thunder Theme Park',
-    address: 'Ooty road, Coimbatore - 641 305',
-    description:
-    'Black Thunder is a water theme park. It is situated at the foot of Nilgiris.',
+    name: 'Harry Paul Melvin',
+    address: 'Team Leader',
+    description: '3.9/5',
     thumbnail:
-    'https://www.prabutourstravels.com/assets/images/large/blackthunder1.jpg',
+    'https://t4.ftcdn.net/jpg/02/23/50/73/240_F_223507324_jKl7xbsaEdUjGr42WzQeSazKRighVDU4.jpg',
     coordinates: LatLng(11.019349, 76.54476),
   ),
   Places(
-    name: 'Maharaja Theme Park',
-    address: 'Coimbatore - 641 062',
-    description:
-    'Maharaja Theme Park is an amusement park that also has movie screens attached with it.',
+    name: 'Harshavarthini',
+    address: 'Team Leader',
+    description: '2.8/5',
     thumbnail:
-    'https://ak.jogurucdn.com/media/image/p25/place-2017-07-14-6-9ded2340da3a4190a43d54913b2ffdae.jpg',
+    'https://static.vecteezy.com/system/resources/thumbnails/001/993/889/small_2x/beautiful-latin-woman-avatar-character-icon-free-vector.jpg',
     coordinates: LatLng(11.0591, 77.0943),
   ),
   Places(
-    name: 'VOC Park & Zoo',
-    address: 'Coimbatore - 641 018',
-    description: 'V.O.Chidambaranar park and zoo is a zoological garden.',
+    name: 'Karthick Naren',
+    address: 'Regional Manager',
+    description: '4.8/5',
     thumbnail:
-    'https://www.holidify.com/images/cmsuploads/compressed/800px-Voc_Zoo_Front_View_20200130105058.jpg',
+    'https://t4.ftcdn.net/jpg/02/23/50/73/240_F_223507324_jKl7xbsaEdUjGr42WzQeSazKRighVDU4.jpg',
     coordinates: LatLng(11.005718, 76.970914),
   ),
 ];
